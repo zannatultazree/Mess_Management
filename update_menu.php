655ef5ae4f1e772fca8decd3e85f554ef458e9be
zannatultazree <zannatultazreeramisa@gmail.com>
2022-09-21 02:10:42 +0600

    Added Navigation bar

---
+++ update_menu.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="update_menu.css">
+</head>
+<body>
+    <nav>
+        <ul class = "nav_item">
+            <li class="nav_list"><a href="#">Home</a></li>
+            <li class="nav_list"><a href="#">Billing</a></li>
+            <li class="nav_list"><a href="#">Food Request</a></li>
+            <li class="nav_list"><a href="#">Member Request</a></li>
+            <li class="nav_list"><a href="#">Update Menu</a></li>
+            <li class="nav_list"><a href="#">Logout</a></li>
+
+        </ul>
+    </nav>
+</body>
+</html>
