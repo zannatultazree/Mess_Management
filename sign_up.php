fc73192d982f9e6c6894c30db72cd0288beafba7
zannatultazree <zannatultazreeramisa@gmail.com>
2022-09-24 22:56:36 +0600

    user_home frontend

---
+++ sign_up.php
@@ -0,0 +1,82 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="sign_up.css">
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Oleo Script Swash Caps">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-iYQeCzEYFbKjA/T2uDLTpkwGzCiq6soy8tYaI1GyVh/UjpbCx/TYkiZhlZB6+fzT" crossorigin="anonymous">
+</head>
+<body>    
+        <div class="bg">
+            <!-- Navbar -->
+<nav class="navbar navbar-expand-lg navbar-dark shadow-5-strong">
+    <!-- Container wrapper -->
+    <div class="container-fluid">
+      <!-- Navbar brand -->
+      <a class="navbar-brand" href="index.html"><img src="BUP LOGO.png" alt="" style="width: 50px;"></a>
+  
+      <!-- Toggle button -->
+      <button
+        class="navbar-toggler"
+        type="button"
+        data-mdb-toggle="collapse"
+        data-mdb-target="#navbarSupportedContent"
+        aria-controls="navbarSupportedContent"
+        aria-expanded="false"
+        aria-label="Toggle navigation"
+      >
+        <i class="fas fa-bars"></i>
+      </button>
+  
+      <!-- Collapsible wrapper -->
+      <div class="collapse navbar-collapse justify-content-end" id="navbarSupportedContent">
+        <ul class="navbar-nav navbar-edit">
+          <li class="nav-item">
+         			
+        </ul>		  
+      </div>
+      <!-- Collapsible wrapper -->
+    </div>
+    <!-- Container wrapper -->
+  </nav>
+  <!-- Navbar -->
+
+
+  <p id="mess" class="display-6">Sign Up</p>
+  <p></p><br><br><br>
+
+  <table align="center" column-gap: 40px;>
+    <tr>
+        <td>
+        <div class = "box1" align="left">
+
+</div>
+        </td>
+
+        <td>
+        <div class = "box1" align="left">
+
+</div>
+        </td>
+    </tr>
+  </table>
+            
+         
+  </body>
+</html>
+
+
+
+
+
+
+            </div>
+
+        </div>
+        <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.6/dist/umd/popper.min.js" integrity="sha384-oBqDVmMz9ATKxIep9tiCxS/Z9fNfEXiDAYTujMAeBAsjFuCZSmKbSSUnQlmh/jp3" crossorigin="anonymous"></script>
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/js/bootstrap.min.js" integrity="sha384-7VPbUDkoPSGFnVtYi0QogXtr74QeVeeIs99Qfg5YCF+TidwNdjvaKZX19NZ/e6oz" crossorigin="anonymous"></script>
+</body>
+</html>
---
+++ user_home.php
@@ -0,0 +1,105 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="user_home.css">
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Oleo Script Swash Caps">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-iYQeCzEYFbKjA/T2uDLTpkwGzCiq6soy8tYaI1GyVh/UjpbCx/TYkiZhlZB6+fzT" crossorigin="anonymous">
+</head>
+<body>    
+        <div class="bg">
+            <!-- Navbar -->
+<nav class="navbar navbar-expand-lg navbar-dark shadow-5-strong">
+    <!-- Container wrapper -->
+    <div class="container-fluid">
+      <!-- Navbar brand -->
+      <a class="navbar-brand" href="index.html"><img src="BUP LOGO.png" alt="" style="width: 50px;"></a>
+  
+      <!-- Toggle button -->
+      <button
+        class="navbar-toggler"
+        type="button"
+        data-mdb-toggle="collapse"
+        data-mdb-target="#navbarSupportedContent"
+        aria-controls="navbarSupportedContent"
+        aria-expanded="false"
+        aria-label="Toggle navigation"
+      >
+        <i class="fas fa-bars"></i>
+      </button>
+  
+      <!-- Collapsible wrapper -->
+      <div class="collapse navbar-collapse justify-content-end" id="navbarSupportedContent">
+        <ul class="navbar-nav navbar-edit">
+          <li class="nav-item">
+            <a class="nav-link active" aria-current="page" href="#">Home</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#">Billing</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#">Food Request</a>
+          </li>	
+          <li class="nav-item">
+            <a class="nav-link" href="#">Member Request</a>
+          </li>	
+          <li class="nav-item">
+            <a class="nav-link" href="#">Update Menu</a>
+          </li>	
+          <li class="nav-item">
+            <a class="nav-link" href="#">LogOut</a>
+          </li>				
+        </ul>		  
+      </div>
+      <!-- Collapsible wrapper -->
+    </div>
+    <!-- Container wrapper -->
+  </nav>
+  <!-- Navbar -->
+            <p id="mess" class="display-6">Officers' Mess</p>
+            <div class="box1" id="bg-img">
+                <h2 align="center">Request Meal</h2>
+
+
+    <form action='https://register-demo.freecodecamp.org'>
+      <fieldset>
+        <label>Number of Meals: <input type="text" name="first-name" required /></label><br>
+        <label>Day
+          <select name="referrer">
+            <option value="">(select day)</option>
+            <option value="1">Sunday</option>
+            <option value="2">Monday</option>
+            <option value="3">Tuesday</option>
+            <option value="4">Wednesday</option>
+            <option value="5">Thursday</option>
+          </select>
+        </label><br>
+        <label>Date: <input type="date" name="last-name" required /></label><br>
+        <label>Meal Type
+          <select name="referrer">
+            <option value="">(select Meal Type)</option>
+            <option value="1">Breakfast</option>
+            <option value="2">Lunch</option>
+            </select>
+        </label>
+      </fieldset>
+      <input type="submit" value="Submit" />
+    </form>
+  </body>
+</html>
+
+
+
+
+
+
+            </div>
+
+        </div>
+        <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.6/dist/umd/popper.min.js" integrity="sha384-oBqDVmMz9ATKxIep9tiCxS/Z9fNfEXiDAYTujMAeBAsjFuCZSmKbSSUnQlmh/jp3" crossorigin="anonymous"></script>
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/js/bootstrap.min.js" integrity="sha384-7VPbUDkoPSGFnVtYi0QogXtr74QeVeeIs99Qfg5YCF+TidwNdjvaKZX19NZ/e6oz" crossorigin="anonymous"></script>
+</body>
+</html>
